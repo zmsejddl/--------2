ea626823a78dd0d8d40b564474a65a87d156637a
Jung Young Hun <hujung0268@gamil.com>
2024-08-27 10:51:27 +0900

    생명주기 함수 사용해 보기

--- my-app/src/chapter_06/Notification.jsx
+++ my-app/src/chapter_06/Notification.jsx
@@ -22,6 +22,18 @@ class Notification extends React.Component {
     this.state = {};
   }
 
+  componentDidMount() {
+    console.log(`${this.props.id} componentDidMount() called.`);
+  }
+
+  componentDidUpdate() {
+    console.log(`${this.props.id} componentDidUpdate() called.`);
+  }
+
+  componentDidUnmount() {
+    console.log(`${this.props.id} componentDidUnmount() called.`);
+  }
+
   render() {
     return (
       <div style={styles.wrapper}>
--- my-app/src/chapter_06/NotificationList.jsx
+++ my-app/src/chapter_06/NotificationList.jsx
@@ -3,12 +3,15 @@ import Notification from "./Notification";
 
 const reservedNotifications = [
   {
+    id: 1,
     message: "안녕하세요, 오늘 일정을 알려드립니다.",
   },
   {
+    id: 2,
     message: "점심 식사 시간입니다.",
   },
   {
+    id: 3,
     message: "이제 곧 미팅이 시작됩니다.",
   },
 ];
@@ -34,6 +37,9 @@ class NotificationList extends React.Component {
           notifications: notifications,
         });
       } else {
+        this.setState({
+          notifications: [],
+        });
         clearInterval(timer);
       }
     }, 1000);
@@ -48,7 +54,13 @@ class NotificationList extends React.Component {
     return (
       <div>
         {this.state.notifications.map((notification) => {
-          return <Notification message={notification.message} />;
+          return (
+            <Notification
+              key={notification.id}
+              id={notification.id}
+              message={notification.message}
+            />
+          );
         })}
       </div>
     );
