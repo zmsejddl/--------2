aa1400a3078d1f8aa66f5c1cc51b931e3e5cc1b9
Jung Young Hun <hujung0268@gamil.com>
2024-08-28 11:57:16 +0900

    클래스 필드 문법 사용하기

--- my-app/src/chapter_08/ConfirmButton.jsx
+++ my-app/src/chapter_08/ConfirmButton.jsx
@@ -1,29 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 
-class ConfirmButton extends React.Component {
-  constructor(props) {
-    super(props);
+function ConfirmButton(props) {
+  const [isConfirmed, setIsConfirmed] = useState(false);
 
-    this.state = {
-      isConfirmed: false,
-    };
-
-    this.handleConfirm = this.handleConfirm.bind(this);
-  }
-
-  handleConfirm = () => {
-    this.setState((prevState) => ({
-      isConfirmed: !prevState.isConfirmed,
-    }));
+  const handleConfirm = () => {
+    setIsConfirmed((prevIsConfirmed) => !prevIsConfirmed);
   };
 
-  render() {
-    return (
-      <button onClick={this.handleConfirm} disabled={this.state.isConfirmed}>
-        {this.state.isConfirmed ? "확인됨" : "확인하기"}
-      </button>
-    );
-  }
+  return (
+    <button onClick={handleConfirm} disabled={isConfirmed}>
+      {isConfirmed ? "확인됨" : "확인하기"}
+    </button>
+  );
 }
 
 export default ConfirmButton;
